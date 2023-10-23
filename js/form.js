b9599ae7cc6e907b259b2d51288953d29894ad5b
MarianoJSB <84196533+MarianoJSB@users.noreply.github.com>
2023-10-23 01:10:57 -0300

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('.form')
+form.addEventListener('submit',eve=>{
+
+    eve.preventDefault()
+
+    const name = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    const message = document.getElementById('text').value
+    const body = `Name:${name}<br>
+    Email:${email}<br>
+    Message:${message}
+    `
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "mariano.n.aguiar@gmail.com",
+        Password : "80352459D6CD1AF3AC344B489223F4DEEE8F",
+        To : 'mariano.n.aguiar@gmail.com',
+        From : 'mariano.n.aguiar@gmail.com',
+        Subject : "Pedido",
+        Body : `${body}`
+    }).then(message => alert(message));
+
+    form.reset();
+})
